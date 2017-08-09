bd58dcddf9ea955a5895d34fe69ea05e7b24ca18
David Wolff <info@davidwolff.dk>
2017-08-09 12:08:13 +0200

    routes init

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/HouseOfCode', {
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/charts', {
 	useMongoClient: true
 });
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,9 @@
 const routes = require('express').Router();
 
 const event = require('./event.js');
-
-// const eventList = require('./eventList.js');
 const annotations = require('./annotations.js');
 
 routes.use('/event', event);
-// routes.use('/eventlist', eventList);
 routes.use('/annotations', annotations);
 
 module.exports = routes;
