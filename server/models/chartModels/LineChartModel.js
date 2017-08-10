bd41fb94076461457d3973a0abf1032b467b3325
David Wolff <info@davidwolff.dk>
2017-08-10 10:22:29 +0200

    working linechart model and end point

--- server/models/chartModels/LineChartModel.js
+++ server/models/chartModels/LineChartModel.js
@@ -3,7 +3,24 @@ const mongoose = require('mongoose');
 let ObjectId = mongoose.Schema.Types.ObjectId;
 
 let LineChart = mongoose.model('LineChart', {
-	
+	_id: { type: ObjectId, required: true },
+	clientId: { type: String, required: true },
+	clientName: { type: String, required: true },
+	title: { type: String, required: true },
+	subtitle: { type: String, required: false },
+	xAxis: {
+		categories: { type: [String], required: false },
+		text: String
+	},
+	yAxis: {
+		text: { type: String, required: false }
+	},
+	series: [{
+		_id: false,
+		name: { type: String, required: true },
+		data: { type: Array, required: true, }
+	}]
+
 });
 
 module.exports = { LineChart };
--- server/routes/chart/lineChart.js
+++ server/routes/chart/lineChart.js
@@ -1,10 +1,49 @@
 const route = require('express').Router();
+const { ObjectId } = require('mongodb');
 
 const { LineChart } = require('../../models/chartModels/LineChartModel.js');
 
 // Get Event List
 route.get('/', (req, res) => {
-	res.send("YAYA");
+	LineChart.find().then( (lineChart) => {
+		res.send(lineChart);
+	}, (e) => {
+		res.status(400)
+			.send(e);
+	});
 });
+
+route.post('/', (req, res) => {
+
+	let objId = new ObjectId();
+
+	let lineChart = new LineChart({
+		_id: objId,
+		clientId: req.body.clientId,
+		clientName: req.body.clientName,
+		title: req.body.title,
+		subtitle: req.body.subtitle,
+		xAxis: {
+			categories: req.body.xAxis.categories,
+			text: req.body.xAxis.text
+		},
+		yAxis: {
+			text: req.body.yAxis.text
+		},
+		series: req.body.series
+	});
+
+	lineChart.save().then( (doc) => {
+		res.status(200)
+			.send(doc);
+	}, (e) => {
+		res.status(400)
+			.send(e);
+	});
+
+});
+
+
+
 
 module.exports = route;
